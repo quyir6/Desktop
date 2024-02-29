16313383c57167c852c36a8c0d842fee05697048
Jack Sullivan <gavinjack.sullivan@icloud.com>
2024-02-28 20:59:58 -0700

    Moorelbr 2/28

---
+++ website/moorelbrhtml/moorejs/moorelbr.js
@@ -0,0 +1,31 @@
+let secondcmmnd = [
+  {
+    ndpic: "/moorelbrhtml/mooreimg/profile.webp",
+    ndtitle: "Yard Manager",
+    ndname: "Ken Vaughn",
+  },
+  {
+    ndpic: "/moorelbrhtml/mooreimg/profile.webp",
+    ndtitle: "Payroll/HR",
+    ndname: "Nancy",
+  },
+  {
+    ndpic: "/moorelbrhtml/mooreimg/profile.webp",
+    ndtitle: "Store Lead",
+    ndname: "Kevin",
+  },
+];
+
+let secondcmmndHTML = "";
+
+secondcmmnd.forEach((ndmemb) => {
+  secondcmmndHTML += `
+   <div class="beside">
+      <img src="${ndmemb.ndpic}" class="teampicture" />
+      <h3 class="ndtitle">${ndmemb.ndtitle}</h3>
+      <p class="ndname">${ndmemb.ndname}</p>
+    </div>
+  `;
+});
+
+document.querySelector(".js-2nd").innerHTML = secondcmmndHTML;
