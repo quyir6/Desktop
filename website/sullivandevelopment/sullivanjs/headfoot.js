8a114333265ad8faa14f80d9d533850d9ac51fc7
Jack Sullivan <gavinjack.sullivan@icloud.com>
2024-03-08 16:03:30 -0700

    hamburger

---
+++ website/sullivandevelopment/sullivanjs/headfoot.js
@@ -0,0 +1,8 @@
+const hamMenu = document.querySelector(".hamburger");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("is-active");
+  offScreenMenu.classList.toggle("active");
+});
